c7584818b558f5fc84d9dc56f8ea5237c8b4c4dc
Nguyen Thanh Trong <ngthtrong0354@gmail.com>
2025-04-26 18:26:59 +0700

    add a station to line - done

--- MetroTicketReservation.API/Controllers/StationLinesController.cs
+++ MetroTicketReservation.API/Controllers/StationLinesController.cs
@@ -1,5 +1,11 @@
 ﻿using MediatR;
+using MetroTicketReservation.Application.Features.Stations.Commands.CreateStation;
+using MetroTicketReservation.Application.Features.Stations.Commands.DeleteStation;
+using MetroTicketReservation.Application.Features.Stations.Commands.UpdateStation;
 using MetroTicketReservation.Application.Features.Stations.Queries.GetAllStations;
+using MetroTicketReservation.Application.Features.StationsLines.Commands.AddStationToLine;
+using MetroTicketReservation.Application.Features.StationsLines.Commands.DeleteStationFromLine;
+using MetroTicketReservation.Application.Features.StationsLines.Commands.UpdateStationOrder;
 using MetroTicketReservation.Application.Features.StationsLines.Queries.GetStationsByLine;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -16,11 +22,33 @@ namespace MetroTicketReservation.API.Controllers
         {
             _mediator = mediator;
         }
-        [HttpGet("{lineId}/stations")]
+        // Get
+        [HttpGet("{lineId}")]
         public async Task<IActionResult> Get(int lineId)
         {
             var result = await _mediator.Send(new GetStationsByLineRequest(lineId));
             return Ok(result);
+        }
+        // CREATE
+        [HttpPost]
+        public async Task<IActionResult> AddStationToLine([FromBody] AddStationToLineRequest request)
+        {
+            await _mediator.Send(request);
+            return StatusCode(StatusCodes.Status201Created);
+        }
+        // PUT
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] UpdateStationOrderRequest request)
+        {
+            await _mediator.Send(request);
+            return NoContent();
+        }
+        // DELETE
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromQuery] DeleteStationFromLineRequest request)
+        {
+            await _mediator.Send(request);
+            return NoContent();
         }
     }
 }
